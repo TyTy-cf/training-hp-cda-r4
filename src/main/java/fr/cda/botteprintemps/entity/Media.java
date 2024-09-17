470f7caa308b12758ec69aab24ed2e5976b6a215
Kevin Tourret <kevin@drosalys.fr>
2024-09-17 16:38:30 +0200

    add entity media

---
+++ src/main/java/fr/cda/botteprintemps/entity/Media.java
@@ -0,0 +1,25 @@
+package fr.cda.botteprintemps.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Table(indexes = { @Index(columnList = "path") })
+public class Media {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String path;
+
+    @Column(nullable = false, length = 12)
+    private String extension;
+
+}
