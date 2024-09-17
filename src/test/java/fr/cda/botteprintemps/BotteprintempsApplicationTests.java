f7de7fd1d643839d47d381ddc2d776356750b327
Kevin Tourret <kevin@drosalys.fr>
2024-09-17 14:35:50 +0200

    first commit

---
+++ src/test/java/fr/cda/botteprintemps/BotteprintempsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cda.botteprintemps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotteprintempsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
