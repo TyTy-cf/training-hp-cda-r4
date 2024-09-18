2b02419b4b6062ddd4972b55abd73bdebbade8cb
Kevin Tourret <kevin@drosalys.fr>
2024-09-18 16:50:31 +0200

    starting ocrrection for exo

---
+++ src/main/java/fr/cda/botteprintemps/entity/redditish/Category.java
@@ -0,0 +1,34 @@
+package fr.cda.botteprintemps.entity.redditish;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.CustomLog;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String slug;
+
+    @ManyToOne
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+
+}
