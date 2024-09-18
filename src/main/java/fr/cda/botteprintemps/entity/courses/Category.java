d653dd7e2b614e9c103a77a7024d80a288795cfc
Kevin Tourret <kevin@drosalys.fr>
2024-09-18 10:47:31 +0200

    coruses data entityy

---
+++ src/main/java/fr/cda/botteprintemps/entity/courses/Category.java
@@ -0,0 +1,26 @@
+package fr.cda.botteprintemps.entity.courses;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Product> products = new ArrayList<>();
+
+}
---
+++ src/main/java/fr/cda/botteprintemps/entity/courses/Product.java
@@ -0,0 +1,33 @@
+package fr.cda.botteprintemps.entity.courses;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+        name = "product_category",
+        joinColumns = @JoinColumn(name = "product_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<Category> categories = new ArrayList<>();
+
+}
+
+
